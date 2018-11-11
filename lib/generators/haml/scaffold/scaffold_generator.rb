191cfa121e3944a90cf69b4f9182a48d299cf4af
shiro <shiro.mns@gmail.com>
2018-11-11 21:19:18 +0900

    [変更]scaffoldのテンプレートをgenerateのhamlファイルを参照するように(設定不足分追加)

--- lib/generators/haml/scaffold/scaffold_generator.rb
+++ lib/generators/haml/scaffold/scaffold_generator.rb
@@ -1,3 +1,9 @@
-class Haml::ScaffoldGenerator < Rails::Generators::NamedBase
+require "rails/generators/erb/scaffold/scaffold_generator"
+class Haml::ScaffoldGenerator < Erb::Generators::ScaffoldGenerator
   source_root File.expand_path('templates', __dir__)
+
+  protected
+  def handler
+    :haml
+  end
 end
