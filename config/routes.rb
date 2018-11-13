ce1b4f62794c22dff0f3bd7814e774a5c123a940
shiro <shiro.mns@gmail.com>
2018-11-13 09:30:44 +0900

    [追加]城状況一覧

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :conditions
   resources :regalia
   resources :specs
   resources :statuses
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_233509) do
+ActiveRecord::Schema.define(version: 2018_11_13_001659) do
+
+  create_table "conditions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "result_no"
+    t.integer "generate_no"
+    t.integer "e_no"
+    t.integer "condition_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["condition_id"], name: "index_conditions_on_condition_id"
+    t.index ["e_no", "result_no", "generate_no"], name: "unique_eno"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "result_no"
