a1a57ef11fa0ad55c18c31c5e55a74085120a0f4
shiro <shiro.mns@gmail.com>
2019-02-16 20:00:01 +0900

    [追加]チェックボックスの性質によって関数を分割

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    def placeholder_set
+        @placeholder = {}
+        @placeholder["Number"] = "例）1~10/50~100"
+        @placeholder["Text"]   = "例）カルマ火炎罠/\"火炎罠\""
+        @placeholder["Name"]   = "例）太郎/\"次郎\""
+    end
 end
