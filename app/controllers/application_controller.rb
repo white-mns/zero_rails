7c54a546a73d1fe236410e78162ef9d6fe6c93df
shiro <shiro.mns@gmail.com>
2019-04-02 02:12:50 +0900

    [追加]フォーム入力例

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    def placeholder_set
+        @placeholder = {}
+        @placeholder["Number"]      = "例）1~10/50~100"
+        @placeholder["Text"]        = "例）罠/\"護衛\""
+        @placeholder["Name"]        = "例）バルーナス/\"ナス\""
+        @placeholder["Item"]        = "例）あなたのユニット"
+        @placeholder["ItemKind"]    = "例）火炎罠/カルマ電撃護衛/"
+        @placeholder["OrigName"]    = "例）ファイター/サキュバス/"
+        @placeholder["Fuka"]        = "例）活力/\"鎮痛\""
+        @placeholder["AddEffect"]   = "例）加速付与/\"遅延\""
+    end
 end
