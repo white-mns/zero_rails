94468e3606a7cf583e66824ae31f388d5dd2c34e
shiro <shiro.mns@gmail.com>
2019-04-03 11:20:00 +0900

    [追加]売上、捕虜の追加。共通関数、Helperの適用

--- app/controllers/transitions_controller.rb
+++ app/controllers/transitions_controller.rb
@@ -4,6 +4,7 @@ class TransitionsController < ApplicationController
 
   # GET /transitions
   def index
+    placeholder_set
     param_set
     @count	= Transition.notnil().includes(:pc_name).search(params[:q]).result.count()
     @search	= Transition.notnil().includes(:pc_name).page(params[:page]).search(params[:q])
@@ -16,6 +17,7 @@ class TransitionsController < ApplicationController
     if !params["is_form"] then
         params["block_no_form"] = "1"
     end
+    placeholder_set
     param_set
     @search	= Transition.notnil().includes(:p_name).page(params[:page]).search(params[:q])
     @search.sorts = 'id asc' if @search.sorts.empty?
@@ -25,12 +27,16 @@ class TransitionsController < ApplicationController
   end
 
   def param_set
+    @form_params = {}
+
     @latest_result = Name.maximum('result_no')
 
     @data_type = {
         1 => "攻撃",
         2 => "支援",
-        3 => "防衛"
+        3 => "防衛",
+        4 => "売上",
+        5 => "捕虜"
     }
 
     params_clean(params)
@@ -38,45 +44,26 @@ class TransitionsController < ApplicationController
         params["result_no_form"] ||= sprintf('%d',@latest_result)
     end
     
-    reference_text_assign(params, "pc_name_name", "pc_name_form")
-    reference_number_assign(params, "result_no", "result_no_form")
-    reference_number_assign(params, "generate_no", "generate_no_form")
-    reference_number_assign(params, "block_no", "block_no_form")
-    reference_number_assign(params, "e_no", "e_no_form")
-    reference_number_assign(params, "turn", "turn_form")
-    reference_number_assign(params, "created_at", "created_at_form")
-    reference_number_assign(params, "act", "act_form")
-    reference_number_assign(params, "data_type", "data_type_form")
-    reference_number_assign(params, "value", "value_form")
+    params_to_form(params, @form_params, column_name: "pc_name_name", params_name: "pc_name_form", type: "text")
+    params_to_form(params, @form_params, column_name: "result_no", params_name: "result_no_form", type: "number")
+    params_to_form(params, @form_params, column_name: "generate_no", params_name: "generate_no_form", type: "number")
+    params_to_form(params, @form_params, column_name: "block_no", params_name: "block_no_form", type: "number")
+    params_to_form(params, @form_params, column_name: "e_no", params_name: "e_no_form", type: "number")
+    params_to_form(params, @form_params, column_name: "turn", params_name: "turn_form", type: "number")
+    params_to_form(params, @form_params, column_name: "turn", params_name: "turn_form", type: "number")
+    params_to_form(params, @form_params, column_name: "acted_at", params_name: "acted_at_form", type: "number")
+    params_to_form(params, @form_params, column_name: "act", params_name: "act_form", type: "number")
+    params_to_form(params, @form_params, column_name: "data_type", params_name: "data_type_form", type: "number")
+    params_to_form(params, @form_params, column_name: "value", params_name: "value_form", type: "number")
  
-    reference_text_assign(params, "pc_name_nickname", "nickname_form")
-    
-    params[:q]["data_type_eq_any"] ||= []
-    if !params["is_form"] then
-        params["type_attack"]  = "on"  
-        params["type_support"] = "on" 
-        params["type_defense"] = "on" 
-    end
+    params_to_form(params, @form_params, column_name: "pc_name_nickname", params_name: "nickname_form", type: "text")
 
-    if params["type_attack"]  == "on" then params[:q]["data_type_eq_any"].push(1) end
-    if params["type_support"] == "on" then params[:q]["data_type_eq_any"].push(2) end
-    if params["type_defense"] == "on" then params[:q]["data_type_eq_any"].push(3) end
-    
-    @pc_name_form = params["pc_name_form"]
-    @result_no_form = params["result_no_form"]
-    @generate_no_form = params["generate_no_form"]
-    @block_no_form = params["block_no_form"]
-    @e_no_form = params["e_no_form"]
-    @turn_form = params["turn_form"]
-    @act_form = params["act_form"]
-    @data_type_form = params["data_type_form"]
-    @value_form = params["value_form"]
-    
-    @type_attack = params["type_attack"]
-    @type_support = params["type_support"]
-    @type_defense = params["type_defense"]
-
-    @nickname_form = params["nickname_form"]
+    checkbox_params_set_query_any(params, @form_params, query_name: "data_type_eq_any",
+                             checkboxes: [{params_name: "type_attack",   value: 1, first_checked: false},
+                                          {params_name: "type_support",  value: 2, first_checked: false},
+                                          {params_name: "type_defense",  value: 3, first_checked: false},
+                                          {params_name: "type_sales",    value: 4, first_checked: false},
+                                          {params_name: "type_prisoner", value: 5, first_checked: false}])
   end
   # GET /transitions/1
   #def show
